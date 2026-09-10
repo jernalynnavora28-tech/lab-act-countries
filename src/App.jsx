f88e1ee475020c326cd0d156c0f889a53067abda
jernalynnavora28-tech <jernalynnavora28@gmail.com>
2026-09-11 00:39:26 +0800

    Task 4: refactor to layout route + Outlet + index route

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
-import AboutPage from "./pages/AboutPage"
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
   // TODO: replace this with a real route table using react-router.
   // For now the app only shows HomePage. Clicking nav links causes a full
   // page reload (or a 404) because there's no client-side routing yet.
   return (
- <Routes>
- <Route path="/" element={<Layout><HomePage /></Layout>} />
- <Route path="/countries" element={<Layout><CountriesPage /></Layout>} />
- <Route path="/bucket-list" element={<Layout><BucketListPage /></Layout>} />
- <Route path="/about" element={<Layout><AboutPage /></Layout>} />
- </Routes>
- );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="countries" element={<CountriesPage />} />
+        <Route path="bucket-list" element={<BucketListPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
